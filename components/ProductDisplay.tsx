ce7df4cd8f118978e4d11c8908c654b1a1d1dd8b
Deren <danhluu.business@gmail.com>
2024-05-13 23:05:44 +0700

    new code 11:05PM 13.5.2024

--- components/ProductDisplay.tsx
+++ components/ProductDisplay.tsx
@@ -3,15 +3,16 @@ import product_rt_2 from '@/assets/images/product_rt_2.png';
 import product_rt_3 from '@/assets/images/product_rt_3.png';
 import product_rt_4 from '@/assets/images/product_rt_4.png';
 import Image from 'next/image';
+import { MdStar } from 'react-icons/md';
 
 const ProductDisplay = ({ product }: any) => {
   return (
     <>
-      <section>
-        <div className=''>
+      <section className=''>
+        <div className='flex flex-col gap-14 xl:flex-row'>
           {/* left side */}
-          <div className=''>
-            <div className=''>
+          <div className='flex gap-x-2'>
+            <div className='flex flex-col gap-[7px] flex-wrap'>
               <Image
                 src={product_rt_1}
                 alt='ProductImage'
@@ -38,6 +39,48 @@ const ProductDisplay = ({ product }: any) => {
             </div>
           </div>
           {/* right side */}
+
+          <div className=''>
+            <h3>{product.name}</h3>
+            <div className=''>
+              <MdStar />
+              <MdStar />
+              <MdStar />
+              <MdStar />
+              <p>(111)</p>
+            </div>
+            <div className=''>
+              <div className=''>{product.old_price}</div>
+              <div className=''>{product.new_price}</div>
+            </div>
+            <div className=''>
+              <h4>Select Size</h4>
+              <div className='flex gap-3 my-3'>
+                <div className='flex items-center justify-center h-10 w-10 ring-2 ring-slate-900/10 cursor-pointer'>
+                  S
+                </div>
+                <div className='flex items-center justify-center h-10 w-10 ring-2 ring-slate-900/10 cursor-pointer'>
+                  M
+                </div>
+                <div className='flex items-center justify-center h-10 w-10 ring-2 ring-slate-900/10 cursor-pointer'>
+                  L
+                </div>
+                <div className='flex items-center justify-center h-10 w-10 ring-2 ring-slate-900/10 cursor-pointer'>
+                  XL
+                </div>
+              </div>
+              <div className=''>
+                <button type='button'>Add to cart</button>
+                <button type='button'>Buy it now</button>
+              </div>
+              <p>
+                <span>Category: </span> Women | Jacket | Winter
+              </p>
+              <p>
+                <span>Tags: </span> Modern | Latest
+              </p>
+            </div>
+          </div>
         </div>
       </section>
     </>
--- components/ProductHd.tsx
+++ components/ProductHd.tsx
@@ -3,7 +3,7 @@ import { TbArrowRight } from 'react-icons/tb';
 const ProductHd = ({ product }: any) => {
   return (
     <>
-      <div className=''>
+      <div className='flex items-center flex-wrap gap-x-2'>
         Home <TbArrowRight /> Shop <TbArrowRight /> {product.category}{' '}
         <TbArrowRight /> {product.name}
       </div>
